30ff7d96d8bf1c76f617490f958163935de15e5e
grandpariley <rileyherman@tuta.io>
2022-04-30 14:20:48 -0600

    plugin start

--- posting.js
+++ posting.js
@@ -1,1 +1,23 @@
-console.log("Turnkey posting plugin loaded successfully")
+window.onload = function () {
+    console.log("Turnkey posting plugin loaded");
+    const turnkeyEl = document.getElementById("turnkey-posting");
+    if (!turnkeyEl) {
+        throw 'Turnkey posting plugin missing matching HTML - please put <div id="turnkey-posting" tk-id="<your tk client id here>"></div> somewhere on the page';
+    }
+    if (!turnkeyEl.getAttribute("tk-id")) {
+        throw 'Turnkey posting plugin missing tk-id - please put <div id="turnkey-posting" tk-id="<your tk client id here>"></div> somewhere on the page';
+    }
+    if (checkTkId(turnkeyEl.getAttribute("tk-id"))) {
+        throw 'Turnkey posting plugin invalid tk-id - please check that the correct tk-id has been provided or register for a tk-id by contacting support';
+    }
+    createButton(turnkeyEl);
+}
+
+function checkTkId(tkId) {
+    const res = await fetch("https://testapi.turnkeyapp.ca/plugin/" + tkId);
+    return res.ok;
+}
+
+function createButton(el) {
+
+}
