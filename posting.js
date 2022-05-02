f41b1481c7cb0b3dc0e8f8be5c10403d5a14da0b
grandpariley <rileyherman@tuta.io>
2022-05-01 19:59:49 -0600

    get me a button, bud

--- posting.js
+++ posting.js
@@ -7,17 +7,28 @@ window.onload = function () {
     if (!turnkeyEl.getAttribute("tk-id")) {
         throw 'Turnkey posting plugin missing tk-id - please put <div id="turnkey-posting" tk-id="<your tk client id here>"></div> somewhere on the page';
     }
-    if (checkTkId(turnkeyEl.getAttribute("tk-id"))) {
+    if (!checkTkId(turnkeyEl.getAttribute("tk-id"))) {
         throw 'Turnkey posting plugin invalid tk-id - please check that the correct tk-id has been provided or register for a tk-id by contacting support';
     }
     createButton(turnkeyEl);
 }
 
-function checkTkId(tkId) {
-    const res = await fetch("https://testapi.turnkeyapp.ca/plugin/" + tkId);
-    return res.ok;
+async function checkTkId(tkId) {
+    // const res = await fetch("https://testapi.turnkeyapp.ca/plugin/" + tkId);
+    // return res.ok;
+    return true;
 }
 
 function createButton(el) {
+    shadow = el.attachShadow({ mode: 'closed' });
+    shadow.innerHTML = `
+        <button id="turnkey-posting-button"
+            onclick="console.log('clicked')"
+            onmouseover="this.style.backgroundColor='#ffffff';this.style.color='#53d3d1';"
+            onmouseout="this.style.backgroundColor='#53d3d1';this.style.color='#ffffff'"
+            style="cursor: pointer; padding: 0.5rem; border: 2px solid #53d3d1; border-radius: 0.25rem; font-weight: 600; color: #ffffff; background-color: #53d3d1; -webkit-transition-duration: 0.4s; transition-duration: 0.4s;">
+            Apply on Turnkey
+        </button>
+    `;
 
 }
